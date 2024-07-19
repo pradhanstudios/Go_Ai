fc4f370b8efeb36e02865ed533a95a0ff3eeb5c6
ritik <flipit001@gmail.com>
2024-07-18 19:51:39 -0700

    added a constants file.

---
+++ src/constants.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdint.h>
+#include <cassert>
+
+// #define RELEASE
+
+inline constexpr int N_ROWS = 19;
+inline constexpr int N_COLS = N_ROWS;
+inline constexpr int N_SQUARES = N_ROWS * N_COLS;
